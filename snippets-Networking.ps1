cd2ac6a37fb1dbfbd4d9c78128b27d0ebe72a402
roysubs <roysubs@hotmail.com>
2022-01-11 18:52:52 +0000

    Update snippets-Networking.ps1

--- snippets-Networking.ps1
+++ snippets-Networking.ps1
@@ -1,3 +1,6 @@
+# Prevent the first run requirement for Internet Explorer for scripting
+# https://stackoverflow.com/questions/38005341/the-response-content-cannot-be-parsed-because-the-internet-explorer-engine-is-no
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize" -Value 2
 
 # Discover the WiFi password currently in use on this host
 $Profile = Netsh wlan show profile name=(Get-NetConnectionProfile -InterfaceAlias WLAN).Name key=clear
