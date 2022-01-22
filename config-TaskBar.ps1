11e9fd4d7a47bdde12c4eadfd3b3f926e6815e76
roysubs <roysubs@hotmail.com>
2022-01-22 22:24:14 +0000

    Update config-TaskBar.ps1

--- config-TaskBar.ps1
+++ config-TaskBar.ps1
@@ -1,5 +1,11 @@
 # Remove junk from new Taskbar and setup my preferences
 ####################
+function Update-Registry ($RegPath, $Item, $Value) {
+    $Path = Split-Path $RegPath ; $Name = Split-Path $RegPath -Leaf
+    if (!(Test-Path -Path $RegPath)) { New-Item -Path $Path -Name $Name -Force }
+    if (!(Test-Path -Path "$RegPath\$Item")) { New-ItemProperty -Path $Path -Name $Item -Value $Value -Force }
+    Set-ItemProperty -Path $RegPath -Name $Item -Value $Value
+}
 
 # Disable "Meet now"
 ####################
@@ -14,18 +20,15 @@
 # Turn off "News and interests" (long method, but comprehensive for silent configuration settings)
 ####################
 # https://silentinstallhq.com/disable-news-and-interests-from-windows-10-taskbar-for-all-users-powershell/
+# https://www.prajwaldesai.com/disable-news-and-interests-in-windows-10/#:~:text=Navigate%20to%3A%20HKEY_CURRENT_USER%5CSoftware%5C,completely%20disable%20News%20and%20Interests.
+Update-Registry "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Feeds" "ShellFeedsTaskbarViewMode" 2
+# Set the value to 2 to completely disable News and Interests.
 
 
 
 # Disable Search box and Cortana on the Taskbar
 ####################
 # https://admx.help/HKLM/SOFTWARE/Policies/Microsoft/Windows/Windows%20Search
-function Update-Registry ($RegPath, $Item, $Value) {
-    $Path = Split-Path $RegPath ; $Name = Split-Path $RegPath -Leaf
-    if (!(Test-Path -Path $RegPath)) { New-Item -Path $Path -Name $Name -Force }
-    if (!(Test-Path -Path "$RegPath\$Item")) { New-ItemProperty -Path $Path -Name $Item -Value $Value -Force }
-    Set-ItemProperty -Path $RegPath -Name $Item -Value $Value
-}
 Update-Registry "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" "AllowCortana" 0
 Update-Registry "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" "SearchboxTaskbarMode" 0
 
@@ -38,6 +41,5 @@ if (((Get-ItemProperty -path $taskbarButtonsRegKey ).TaskbarGlomLevel ) -Ne 2) {
     Set-ItemProperty -Path $taskbarButtonsRegKey -Name "TaskbarGlomLevel" -Value 00000002
 }
 
-Stop-Process -Name Explorer
-& Explorer.exe
+Stop-Process -Name Explorer; & Explorer.exe
 
